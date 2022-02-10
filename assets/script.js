55728d21fb9a5bb20e0c3e5c219f2ae0f6f7d191
esraa mohamed <76407362+esraa544@users.noreply.github.com>
2022-02-10 20:39:15 +0200

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+let menuList=document.getElementById("menuList");
+let menuIcon=document.getElementById("menuIcon");
+            
+menuIcon.addEventListener("click", function togglemenu(){
+    menuList.classList.toggle("res-heigth");
+            } );
